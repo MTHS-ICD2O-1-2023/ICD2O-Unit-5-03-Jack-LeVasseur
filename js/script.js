cf894f31a39716f510f64f40fb3fe0f7a4dd32f4
Jack LeVasseur <159185068+Jack-LeVasseur@users.noreply.github.com>
2024-05-13 22:09:51 +0000

    Updated index.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created on: April 28, 2024
 // This file contains the JS functions for index.html
+
+function enterClicked() {
+  // input
+  var age = promptNum.getElementById("How many years old are you?") 
+  if(age >= 17) {}
+    console.log("You can seen an R movie alone.")
+  } else if(age >= 13) {}
+   console.log("you can see a PG-13 movie alone.")
+  {} else if (age >= 5) {}
+   console.log("You can see a G or PG movie alone.")
+  {} else {}
+    console.log("Uh. You're too young for most things.")
+  {}
+  console.log("Thanks for verifying your age!")
